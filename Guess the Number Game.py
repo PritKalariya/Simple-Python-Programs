93e5cd210ef66af87993299e9823e5d895b8f8f9
Prit Kalariya <kalariyaprit2000@gmail.com>
2020-12-11 20:09:23 +0530

    Added emojis.

--- Guess the Number Game.py
+++ Guess the Number Game.py
@@ -1,25 +1,34 @@
 import random
+import emoji
 
-print("="*15,"NUMBER GUESSING GAME", "="*15)
+print("="*15,"\N{WHITE RIGHT POINTING BACKHAND INDEX} NUMBER GUESSING GAME \N{WHITE LEFT POINTING BACKHAND INDEX}", "="*15)
 
 number = random.randint(1, 9)
 chances = 0
-print("-"*52)
-times = int(input("Enter how many chances you want: "))
-print("-"*52)
+print("\n")
+times = int(input("\N{BOMB} Enter how many chances you want: "))
+print("\n")
 print("GUESS A NUMBER (Between 1 to 9): ")
 
 while chances < times:
+    print("\n")
     guess = int(input("ENTER NUMBER: "))
     if guess == number:
-        print("="*10, "HURRAY!! YOU WON!! :)", "="*10)
+        print("\n")
+        print("="*10, "\N{party popper} HURRAY!! YOU WON!! \N{party popper}", "="*10)
+        print("\n")
         break
     elif guess < number:
-        print(f"Your guess is too low: Guess a higher number than {guess}")
+        print("\n")
+        print(f"\t \N{CROSS MARK} Your guess is too low: Guess a higher number than {guess}")
         chances += 1
     else:
-        print(f"Your guess is too high: Guess a lower number than {guess}")
+        print("\n")
+        print(f"\t \N{CROSS MARK} Your guess is too high: Guess a lower number than {guess}")
         chances += 1
 if not chances < times:
-    print("="*10, "OOPS! YOU LOOSE!! :(", "="*10)
-    print("*"*10, f"The number was {number}", "*"*10)
+    print("\n")
+    print("="*10, "\N{DISAPPOINTED FACE} OOPS! YOU LOOSE!! \N{DISAPPOINTED FACE}", "="*10)
+    print("\n")
+    print("*"*10, f"\N{EYES} The number was {number} \N{EYES}", "*"*10)
+    print("\n")
