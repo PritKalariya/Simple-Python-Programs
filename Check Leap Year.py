decda80b267b6ab547fde88f468c1c8c6f2104aa
Prit Kalariya <kalariyaprit2000@gmail.com>
2020-12-25 22:59:37 +0530

    check leap year.

---
+++ Check Leap Year.py
@@ -0,0 +1,13 @@
+#Python program to check leap year
+year = int(input("Enter year: "))
+
+if(year % 4) == 0:
+    if(year % 100) == 0:
+        if(year % 400) == 0:
+            print("Leap year.")
+        else:
+            print("Not a leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not a leap year.")
