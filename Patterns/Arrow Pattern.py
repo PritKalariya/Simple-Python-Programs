fd4d6219c0a80672bde14197159dc35b63c069ed
Prit Kalariya <kalariyaprit2000@gmail.com>
2021-04-24 22:12:02 +0530

    Arrow Pattern.

---
+++ Patterns/Arrow Pattern.py
@@ -0,0 +1,15 @@
+def arrow(n):
+    for i in range(n):
+        if i == n-1:
+            print((2 * n) * "*", end="")
+            print((i + 1) * "*")
+        else:
+            print((2 * n) * " ", end="")
+            print((i + 1) * "*")
+
+    for j in range(n - 1, 0, -1):
+        print((2 * n) * " ", end="")
+        print(j * "*")
+
+
+arrow(6)
